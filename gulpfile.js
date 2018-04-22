700ca113070146057af3395c755956026c2bf508
dylanburkey <dylanburkey@outlook.com>
2018-04-22 17:17:49 -0400

    Add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,97 @@
+var gulp = require('gulp');
+plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var imagemin = require('gulp-imagemin');
+var sass = require('gulp-sass');
+var minifycss = require('gulp-minify-css');
+var htmlmin = require('gulp-htmlmin');
+var autoprefixer = require('gulp-autoprefixer');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var nunjucksRender = require('gulp-nunjucks-render');
+var newer = require('gulp-newer');
+var data = require('gulp-data');
+
+
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        },
+    })
+})
+
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('minify', function() {
+    return gulp.src('./*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('dist/sandbox/'));
+});
+
+gulp.task('nunjucks', function() {
+    return gulp.src('src/pages/**/*.+(html|njk|nunjucks)')
+    // We do not need the data.json for this demo but you can use it if you wanna
+        .pipe(data(function(){
+        return require('./src/data/data.json');
+        }))
+        .pipe(nunjucksRender({
+            path: ['src/templates/']
+        }))
+        .pipe(gulp.dest('./'))
+
+});
+
+
+
+gulp.task('images', function(){
+    return gulp.src('src/images/**/*.+(png|jpg|gif|svg)')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+});
+
+
+gulp.task('sass', function() {
+  gulp.src(['src/styles/**/*.scss'])
+      .pipe(sass({
+          outputStyle: 'compressed'
+      }).on('error', sass.logError))
+        .pipe(autoprefixer('last 3 versions'))
+        .pipe(gulp.dest('dist/styles/'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('dist/styles/'))
+         .pipe(browserSync.reload({stream:true}))
+});
+gulp.task('scripts', function(){
+    return gulp.src('src/scripts/**/*.js')
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }}))
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('main.js'))
+        .pipe(babel())
+        .pipe(gulp.dest('dist/scripts/'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/scripts/'))
+        .pipe(browserSync.reload({stream:true}))
+});
+
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+    gulp.watch('src/styles/**/*.scss', ['sass']);
+    gulp.watch("src/scripts/**/*.js", ['scripts']);
+    gulp.watch("*.html", ['bs-reload']);
+    // Other watchers
+});
